060ac7a5791c70987806ea9a19891c0e1607e1aa
Charmmy-ing <lc5207242@gmail.com>
2026-08-20 20:36:32 +0800

    8/21v1

---
+++ JavaCourse/src/JavaFoundationalCourse/LoopStructure.java
@@ -0,0 +1,17 @@
+package JavaFoundationalCourse;
+
+public class LoopStructure {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        for(int a:arr){
+            System.out.print(a);
+            System.out.print("-");
+        }
+        System.out.println();
+        String[] Stringarr={"a","b","c","d","e"};
+        for(String s:Stringarr){
+            System.out.print(s);
+            System.out.print("-");
+        }
+    }
+}
---
+++ JavaCourse/src/JavaFoundationalCourse/Operator.java
@@ -0,0 +1,15 @@
+package JavaFoundationalCourse;
+
+public class Operator {
+        public static void main(String[] args){
+//            int num1=2025;
+//            boolean b1=(num1<2026)?true:false;
+//            int num2=2026;
+//            boolean b2=(num2<2022)?true:false;
+//            System.out .println(b1+" "+b2);
+           // ------------------------
+            String counter="65";
+            boolean b=counter instanceof String;
+            System.out.println(b);
+        }
+}
