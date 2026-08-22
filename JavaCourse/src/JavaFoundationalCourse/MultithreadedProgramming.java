3842540a7e3c41c1d00fa7f09ea7e1d2f70f7fc1
Charmmy-ing <lc5207242@gmail.com>
2026-08-22 16:56:04 +0800

    8/22v4

--- JavaCourse/src/JavaFoundationalCourse/MultithreadedProgramming.java
+++ JavaCourse/src/JavaFoundationalCourse/MultithreadedProgramming.java
@@ -1,5 +1,7 @@
 package JavaFoundationalCourse;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 
 public class MultithreadedProgramming {
@@ -8,8 +10,14 @@ public class MultithreadedProgramming {
 //        new Runnabledemo("Thread--1").start();
 //        new Runnabledemo("Thread--2").start();
 //        }
-        new Thread(new  threaddemo("thread1")).start();
-       
+        //new Thread(new  threaddemo("thread1")).start();
+        Callable c=new demo(50) ;
+        FutureTask ft=new FutureTask(c) ;
+        new Thread(ft).start();
+        try {
+            System.out.println(ft.get());
+        }catch (Exception e){
+        e.printStackTrace();}
     }
 }
 //    class Runnabledemo implements Runnable{
@@ -38,36 +46,51 @@ public class MultithreadedProgramming {
 //           t=new Thread(this,name);
 //           t.start();
 //        }
-        class threaddemo extends Thread {
-            private Thread t;
-            private String name;
-
-            public threaddemo(String name) {
-                this.name = name;
-            }
-
-            @Override
-            public void run() {
-                System.out.println("线程启动");
-                for (int i = 0; i <= 100; i++) {
-                    System.out.println(i);
-                    try {
-                        t.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println(name + "线程存在");
-            }
-
-            @Override
-            public void start() {
-                if (t == null) {
-                    t.start();
-                }
-            }
+//        class threaddemo extends Thread {
+//            private Thread t;
+//            private String name;
+//
+//            public threaddemo(String name) {
+//                this.name = name;
+//            }
+//
+//            @Override
+//            public void run() {
+//                System.out.println("线程启动");
+//                for (int i = 0; i <= 100; i++) {
+//                    System.out.println(i);
+//                    try {
+//                        t.sleep(500);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                System.out.println(name + "线程存在");
+//            }
+//
+//            @Override
+//            public void start() {
+//                if (t == null) {
+//                    t.start();
+//                }
+//            }
+//        }
+//----------实现Callable接口，调用FutureTask的get方法得到结果-------
+class demo implements Callable<String> {
+    private int nums;
+    public demo(int nums) {
+        this.nums = nums;
+    }
+    @Override
+    public String call() throws Exception {
+        int n=0;
+        for(int j=1;j<nums;j++){
+            n=n+j;
         }
+        return "From one add to "+nums+" end is "+n+";";
+    }
 
+}
 
 
 
--- JavaCourse/src/JavaFoundationalCourse/Reflection.java
+++ JavaCourse/src/JavaFoundationalCourse/Reflection.java
@@ -1,4 +1,20 @@
 package JavaFoundationalCourse;
+import java.util.*;
 
 public class Reflection {
+    public static void main(String[] args) throws Exception {
+        Student pre1=new Student("CanLu");
+        Class c1=pre1.getClass();
+        Class c2=Student.class;
+        Class c3=Class.forName("JavaFoundationalCourse.Reflection.Student");
+        System.out.println(c1==c2);
+        System.out.println(c1==c3);
+    }
+}
+class Student{
+    String name;
+    int age;
+    public Student(String name){
+        this.name=name;
+    }
 }
